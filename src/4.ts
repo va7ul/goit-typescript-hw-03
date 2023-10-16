448a592c68e3cbe61270a556eca0379085d9354b
va7ul <vas7ul@gmail.com>
2023-10-16 21:00:08 +0300

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,62 @@
+class Key {
+  private signature: number = Math.random();
+
+  // constructor(private signature: number) {
+  //   this.signature = Math.random();
+  // }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      return console.log('Welcome!');
+    }
+    console.log('The door is closed');
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      console.log(key);
+      console.log(this.key);
+
+      this.door = true;
+      return console.log('The door is opened.');
+    }
+    console.log('Invalid key. The door remains closed.');
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +65,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
